171e6c9b1b0e635f5ff8a1f8f7c285d30e0e1be8
gidwikintan <gidwikintan@gmail.com>
2021-09-21 12:52:35 +0700

    integrate table from ant design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 import './App.css';
+import { Table } from "antd";
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 
 function App() {
+  const [rate, setRate] = useState(['CAD', 'IDR', 'JPY', 'CHF', 'EUR', 'USD']);
+  const [state, setstate] = useState([]);
+  const [loading, setloading] = useState(true);
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    await Axios.get("http://api.exchangeratesapi.io/v1/latest?access_key=18cf0cc8c3d1cb752ec2e885a38bc774").then(
+      res => {
+        setloading(false);
+        console.log(res.data)
+        setstate(
+          rate.map(row => ({
+            rate: row,
+          }))
+        );
+      }
+    );
+  };
+
+  const columns = [
+    {
+      title: "",
+      dataIndex: "rate",
+      width: 150
+    },
+    {
+      title: "We Buy",
+      dataIndex: "WeBuy",
+      width: 150
+    },
+    {
+      title: "Exchange Rate",
+      dataIndex: "ExchangeRate",
+      width: 150
+    },
+    {
+      title: "We Sell",
+      dataIndex: "WeSell",
+      width: 150
+    }
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading ? (
+        "Loading"
+      ) : (
+        <Table
+          columns={columns}
+          dataSource={state}
+        />
+      )}
     </div>
   );
 }
